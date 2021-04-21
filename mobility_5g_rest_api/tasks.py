9db14bd76f89fa728d5d047832b7744879ecd089
hugofpaiva <hugofpaiva@ua.pt>
2021-04-21 02:24:31 +0100

    Some more fixes and demonstration of model creation on task

--- mobility_5g_rest_api/tasks.py
+++ mobility_5g_rest_api/tasks.py
@@ -4,14 +4,19 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
+from mobility_5g_rest_api.models import Event
+
 logger = get_task_logger(__name__)
 
 
 @shared_task(track_started=True)
-def add(x, y):
-    print(x + y)
-    return
+def sensor_fusion(json):
+    # Do the sensor fusion
+    print(json)
 
+    ev = Event.objects.create(location="DN",
+                              event_type="RT",
+                              event_class="CA",
+                              velocity=250)
 
-def sum(x,y):
-    print(x+y)
+    return "{} created {}".format(json, ev)
