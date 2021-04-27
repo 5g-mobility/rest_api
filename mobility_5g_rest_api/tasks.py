f2f3510c2eef92b2f95f5282958121c39d11b756
hugofpaiva <hugofpaiva@ua.pt>
2021-04-27 14:34:21 +0100

    Remove old tasks

--- mobility_5g_rest_api/tasks.py
+++ mobility_5g_rest_api/tasks.py
@@ -3,17 +3,8 @@ from __future__ import absolute_import, unicode_literals
 from mobility_5g_rest_api.models import Event
 from celery import shared_task
 from celery.utils.log import get_task_logger
+
 logger = get_task_logger(__name__)
-
-
-@shared_task()
-def add(x, y):
-    return x + y
-
-@shared_task()
-def print_json(json):
-    print(json)
-    return json
 
 @shared_task(track_started=True)
 def sensor_fusion(json):
@@ -26,5 +17,4 @@ def sensor_fusion(json):
                               velocity=250)
 
 
-    # return "Celery has imported {0} tweets from Twitter.".format(n)
     return "{} created {}".format(json, ev)
