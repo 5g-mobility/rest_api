72743eb4f3178f99c4d740445b33e968aabe6f1c
hugofpaiva <hugofpaiva@ua.pt>
2021-05-09 12:52:13 +0100

    Fix Serializers

--- mobility_5g_rest_api/serializers.py
+++ mobility_5g_rest_api/serializers.py
@@ -6,7 +6,7 @@ from mobility_5g_rest_api.models import Event, Climate, DailyInflow
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('timestamp', 'location', 'event_type', 'event_class', 'velocity', 'latitude', 'longitude', 'co2km',
+        fields = ('timestamp', 'location', 'event_type', 'event_class', 'velocity', 'latitude', 'longitude', 'co2',
                   'temperature')
 
 
