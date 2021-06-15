aee788a82783652687134288e5749e851962ab6c
hugofpaiva <hugofpaiva@ua.pt>
2021-06-15 19:23:57 +0100

    Make migrations on start

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 nc -zvv mongodb 27017
 
 python manage.py crontab add
-python manage.py collectstatic --noinput
 python manage.py makemigrations
+python manage.py collectstatic --noinput
 python manage.py migrate
 service nginx start
 gunicorn rest_api.wsgi -b 0.0.0.0:9000
